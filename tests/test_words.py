34d9cb50a53cf1d275fded46dab7d1509b3881a9
Andrew Darby <andrew.darby@citywonders.com>
2020-12-08 19:44:18 +0000

    remove sample test files

--- tests/test_words.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from modules.words import filter_words, display_list
-from modules.input import validate_position
-
-class MyTest(unittest.TestCase):
-    def test_filter_words_returns_correct_output(self):
-        l = ['hello','are','cat','dog','ham','hi','go','to','heart']
-        self.assertEqual(filter_words(l,'h'), ['hello','ham','hi','heart'])
-
